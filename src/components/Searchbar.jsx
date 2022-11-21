2fe0b1c1e25f38d7350eaf55e962fffa4aa91b1f
Omotoke <96818824+GirlWho-Codes@users.noreply.github.com>
2022-11-21 21:58:35 +0100

    Add files via upload

---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
+
+import {FiSearch} from 'react-icons/fi';
+
+const Searchbar = () => {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    navigate(`/search/${searchTerm}`)
+  }
+
+  return(
+  <form onSubmit = {handleSubmit} autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
+    <label htmlFor="search-field" className="sr-only">Search songs</label>
+    <div className="flex flex-row justify-start items-center">
+      <FiSearch className="w-5 h-5 ml-4"/>
+      <input 
+      type="search"
+      name="search-field"
+      autoComplete="off" 
+      id="search-field"
+      placeholder="Search"
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+      className="flex-1 bg-transparent border-none outline-none placeholder-gray-400
+      text-base text-white p-4"
+      />
+    </div>
+  </form>
+)};
+
+export default Searchbar;
